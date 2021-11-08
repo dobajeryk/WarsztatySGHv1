8d074bcb4c3b0822d042b653754545ac38303059
Eryk Dobaj <dobajeryk@gmail.com>
2021-11-08 22:12:29 +0100

    Fixed App

--- src/main/java/com/skni/warsztatysghv1/utils/NotConfiguration.java
+++ src/main/java/com/skni/warsztatysghv1/utils/NotConfiguration.java
@@ -1,7 +1,9 @@
 package com.skni.warsztatysghv1.utils;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class NotConfiguration {
 
     @Bean
