487d00c8d8141f81812e5c981e8cbe041f03cabd
Eryk Dobaj <dobajeryk@gmail.com>
2021-11-08 21:56:14 +0100

    Added service bean (outside main package) with default constructors

---
+++ src/main/java/SomeService.java
@@ -0,0 +1,8 @@
+import org.springframework.stereotype.Service;
+
+@Service
+public class SomeService {
+    public SomeService() {
+        System.out.println("Tworze beana - SomeService");
+    }
+}
