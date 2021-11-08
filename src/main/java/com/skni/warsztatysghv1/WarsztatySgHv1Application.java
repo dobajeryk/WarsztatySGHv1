b58eb892e924a3504b25baa2fa6eb47c5dc99cd6
Eryk Dobaj <dobajeryk@gmail.com>
2021-11-08 21:50:56 +0100

    Initial commit

---
+++ src/main/java/com/skni/warsztatysghv1/WarsztatySgHv1Application.java
@@ -0,0 +1,13 @@
+package com.skni.warsztatysghv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarsztatySgHv1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarsztatySgHv1Application.class, args);
+    }
+
+}
