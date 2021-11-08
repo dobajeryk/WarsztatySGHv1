04cca6b5da35338c7ea6c997942a1494cb1341d5
Eryk Dobaj <dobajeryk@gmail.com>
2021-11-08 22:06:52 +0100

    Broken app -> commented @Service

--- src/main/java/com/skni/warsztatysghv1/stereotypes/ServiceExample.java
+++ src/main/java/com/skni/warsztatysghv1/stereotypes/ServiceExample.java
@@ -2,7 +2,7 @@ package com.skni.warsztatysghv1.stereotypes;
 
 import org.springframework.stereotype.Service;
 
-@Service
+//@Service
 public class ServiceExample {
     public ServiceExample(RepositoryExample repository) {
         System.out.println("Tworze beana - ServiceExample");
