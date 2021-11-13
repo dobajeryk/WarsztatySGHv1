2e84f7685497503cd4930617c0ad9ee199f463d1
Eryk Dobaj <dobajeryk@gmail.com>
2021-11-13 19:09:55 +0100

    Removed MyCutsomBean as parameter from RespositoryExample and ServiceExample (look at the order of creating beans)

--- src/main/java/com/skni/warsztatysghv1/stereotypes/RepositoryExample.java
+++ src/main/java/com/skni/warsztatysghv1/stereotypes/RepositoryExample.java
@@ -1,11 +1,10 @@
 package com.skni.warsztatysghv1.stereotypes;
 
-import com.skni.warsztatysghv1.utils.MyCustomBean;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class RepositoryExample {
-    public RepositoryExample(MyCustomBean myCustomBean) {
+    public RepositoryExample() {
         System.out.println("Tworze beana - RepositoryExample");
     }
 }
--- src/main/java/com/skni/warsztatysghv1/stereotypes/ServiceExample.java
+++ src/main/java/com/skni/warsztatysghv1/stereotypes/ServiceExample.java
@@ -1,11 +1,10 @@
 package com.skni.warsztatysghv1.stereotypes;
 
-import com.skni.warsztatysghv1.utils.MyCustomBean;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ServiceExample {
-    public ServiceExample(RepositoryExample repository, MyCustomBean myCustomBean) {
+    public ServiceExample(RepositoryExample repository) {
         System.out.println("Tworze beana - ServiceExample");
     }
 }
