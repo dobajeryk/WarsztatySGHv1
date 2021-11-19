72ec52a6b636b8d15eeb9cc1e10454931c7c020b
Eryk Dobaj <dobajeryk@gmail.com>
2021-11-19 18:17:51 +0100

    MyComponentNew extends intreface

--- src/main/java/com/skni/warsztatysghv1/di/MyCoponentNew.java
+++ src/main/java/com/skni/warsztatysghv1/di/MyCoponentNew.java
@@ -3,9 +3,9 @@ package com.skni.warsztatysghv1.di;
 import org.springframework.stereotype.Component;
 
 @Component
-public class MyComponent2 implements MyComponent {
+public class MyCoponentNew implements MyComponent {
     @Override
     public void test() {
-        System.out.println("Wstrzyknieto bean (MyComponent2)");
+        System.out.println("Wstrzyknieto bean (MyComponentNew)");
     }
 }
